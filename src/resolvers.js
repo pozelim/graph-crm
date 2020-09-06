fc683320e086e893d292558eade47ff6c4142c01
Pedro Ozelim <pedrohozelim@gmail.com>
2020-09-06 10:21:44 -0300

    Add create user mutation

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,7 @@
 /* src/resolvers.js */
 
+import { v4 as uuidv4 } from 'uuid';
+
 const resolvers = {
   users: async (_, context) => {
     const { db } = await context();
@@ -8,6 +10,12 @@ const resolvers = {
   user: async ({ id }, context) => {
     const { db } = await context();
     return db.get('users').find({ id }).value();
+  },
+  createUser: async ({ input }, context) => {
+    const { db } = await context();
+    const user = { id: uuidv4(), name: input.name, email: input.email };
+    db.get('users').push(user).write();
+    return user;
   },
 };
 
--- src/schema.js
+++ src/schema.js
@@ -5,22 +5,22 @@ import { buildSchema } from 'graphql';
 const schema = buildSchema(`
     type Query {
         users: [User!]!,
-        user(id: Int!): User!
+        user(id: String!): User!
+    }
+
+    input UserInput {
+        name: String
+        email: String
+    }
+
+    type Mutation {
+        createUser(input: UserInput): User
     }
 
     type User {
         id: ID! 
         name: String!
         email: String
-        posts: [Post!]
-    }
-
-    type Post {
-        id: ID!
-        title: String!
-        published: Boolean!
-        link: String
-        author: User!
     }
 `);
 
